9cb13ca57861758e80e6de126e262dc7d87b1649
tm-const <dev@mannyquintanilla.com>
2023-09-03 15:36:28 -0500

    SuccessPG Pass Stripe JSON to fetchDuration()
    Passes fetchDuration() meta to fetch('/get-server-timer-state') else if serverTimerState.timerDuration is false

--- public/fetch-duration.js
+++ public/fetch-duration.js
@@ -8,39 +8,21 @@ const sessionId = urlParams.get('session_id');
 fetch(`/retrieve-session?sessionId=${sessionId}`)
     .then(response => response.json())
     .then(data => {
-        // console.log('Stripe Response:', data);
+        console.log('Stripe Response:', data);
         // You can access the Stripe response data here and perform further actions
+        const startButton = document.getElementById('start-button');
+        var dataRes = data.metadata.duration;
+        const durationVars = [dataRes];
+        const durationArray = durationVars[0];
 
-        if (data.metadata.duration) {
-            const startButton = document.getElementById('start-button');
-            var dataRes = data.metadata.duration;
-            const durationVars = [dataRes];
+        // Split the string into an array of numbers
+        const durationNumArr = durationArray.split(', ').map(Number);
 
-            const durationArray = durationVars[0];
+        // Start the client timer
+        const initialDuration = durationNumArr[0] * 24 * 60 * 60 + durationNumArr[1] * 60 * 60 + durationNumArr[2] * 60 + durationNumArr[3]; // Replace with your desired countdown duration
+        startClientTimer(initialDuration);
 
-            // Split the string into an array of numbers
-            const durationNumArr = durationArray.split(', ').map(Number);
-            console.log(durationNumArr[0])
-            console.log('Stripe Response:', [data.metadata.duration]);
+        // Hide the start button after starting the timer
+        startButton.style.display = 'none';
 
-            // Start the client timer
-            const initialDuration = durationNumArr[0] * 24 * 60 * 60 + durationNumArr[1] * 60 * 60 + durationNumArr[2] * 60 + durationNumArr[3]; // Replace with your desired countdown duration
-            startClientTimer(initialDuration);
-
-            // Hide the start button after starting the timer
-            startButton.style.display = 'none';                    
-
-        } else {
-            // Event listener for the Start Countdown button
-            startButton.addEventListener('click', () => {
-                // Start the client timer
-                const initialDuration = 3 * 24 * 60 * 60 + 1 * 60 * 60 + 3 * 60 + 50; // Replace with your desired countdown duration
-                startClientTimer(initialDuration);
-
-                // Hide the start button after starting the timer
-                startButton.style.display = 'none';
-            });
-        }
-    })
-    .catch(error => console.error('Error:', error));
-}
+    }).catch(error => console.error('Error:', error));}
